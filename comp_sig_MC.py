92376814a5377cc2430a3db2307edbea5f3bbd67
V <44793625+LucianoAndrian@users.noreply.github.com>
2021-12-30 13:39:55 -0300

    Add files via upload
    act

--- comp_sig_MC.py
+++ comp_sig_MC.py
@@ -44,23 +44,29 @@ i = '1950'
 s = seasons[0]
 
 
-def NumberPerts(data_to_concat, neutro):
+def NumberPerts(data_to_concat, neutro, num = 0):
     total = len(data_to_concat) + len(neutro)
     len1 = len(neutro)
     len2 = len(data_to_concat)
 
     total_perts = math.factorial(total) / (math.factorial(len2) * math.factorial(len1))
 
-    if total_perts >= 10000:
-        tot = 10000
-        print('M = 10000')
+    if num == 0:
+
+        if total_perts >= 10000:
+            tot = 10000
+            print('M = 10000')
+        else:
+            tot = total_perts
+            print('M = ' + str(total_perts))
+        jump = 99
     else:
-        tot = total_perts
-        print('M = ' + str(total_perts))
+        tot = num
+        jump = 9
 
     M = []
     n = 0
-    jump = 9
+
     while n < tot:
         aux = list(np.linspace((0 + n), (n + jump), (jump + 1)))
         M.append(aux)
@@ -68,23 +74,6 @@ def NumberPerts(data_to_concat, neutro):
 
     return M
 
-def PermuDatesComposite(n, data=data, mmonth=mmonth, season_name=s):
-    mmin = mmonth[0]
-    mmax = mmonth[-1]
-    rn = np.random.RandomState(616)
-    for a in n:
-        dates_rn = rn.permutation(neutro_concat)
-        neutro_new = dates_rn[0:len(neutro)]
-        data_new = dates_rn[len(neutro):]
-
-        neutro_comp = CompositeSimple(original_data=data, index=neutro_new,
-                                      mmin=mmin, mmax=mmax)
-        data_comp = CompositeSimple(original_data=data, index=data_new,
-                                    mmin=mmin, mmax=mmax)
-
-        comp = data_comp - neutro_comp
-        comp.to_netcdf('/datos/luciano.andrian/ncfiles/nc_comps/' + 'Comps_' +
-                       str(int(a)) + '_' + season_name + '.nc')
 
 
 def ParallelProc(M, excluded_processors=15):
@@ -92,128 +81,70 @@ def ParallelProc(M, excluded_processors=15):
     pool.map_async(PermuDatesComposite, [n for n in M])
     pool.close()
 
+cases = ['DMI_sim_pos', 'DMI_sim_neg', 'DMI_neg', 'DMI_pos', 'DMI_un_pos', 'DMI_un_neg',
+         'N34_pos', 'N34_neg', 'N34_un_pos', 'N34_un_neg']
+c = cases[0]
+for c in cases:
+    for i in start:
+        print(i)
+        data = xr.open_dataset(data_dir + variable + '.nc')
+        print('Open ' + variable + '.nc')
 
-for i in start:
-    print(i)
-    data = xr.open_dataset(data_dir + variable + '.nc')
-    print('Open ' + variable + '.nc')
+        # ------------------------------------------------------------------------------------------------------------------#
+        if len(data.sel(lat=slice(-90, 20)).lat.values) == 0:
+            data = data.sel(time=slice(i + '-01-01', '2020-12-01'), lat=slice(20, -90))
+        else:
+            data = data.sel(time=slice(i + '-01-01', '2020-12-01'), lat=slice(-90, 20))
+        # ------------------------------------------------------------------------------------------------------------------#
+        count = 0
+        for s in seasons:
 
-    #------------------------------------------------------------------------------------------------------------------#
-    if len(data.sel(lat=slice(-90, 20)).lat.values) == 0:
-        data = data.sel(time=slice(i + '-01-01', '2020-12-01'), lat=slice(20, -90))
-    else:
-        data = data.sel(time=slice(i + '-01-01', '2020-12-01'), lat=slice(-90, 20))
-    #------------------------------------------------------------------------------------------------------------------#
-    count = 0
-    for s in seasons:
-
-        mmonth = min_max_months[count]
-
-        aux = xr.open_dataset(nc_date_dir + 'Composite_' + i + '_2020_' + s + '.nc')
-        neutro = aux.Neutral
-        data_to_concat = aux.DMI_sim_pos
-        del aux
-
-        M = NumberPerts(data_to_concat, neutro)
-
-        if data_to_concat[0] != 0:
-            neutro_concat = np.concatenate([neutro, data_to_concat])
-
-            ParallelProc(M, 15)
+            mmonth = min_max_months[count]
 
 
-########################################################################################################################333333
+            def PermuDatesComposite(n, data=data, mmonth=mmonth, season_name=s):
+                mmin = mmonth[0]
+                mmax = mmonth[-1]
+                rn = np.random.RandomState(616)
+                for a in n:
+                    dates_rn = rn.permutation(neutro_concat)
+                    neutro_new = dates_rn[0:len(neutro)]
+                    data_new = dates_rn[len(neutro):]
+
+                    neutro_comp = CompositeSimple(original_data=data, index=neutro_new,
+                                                  mmin=mmin, mmax=mmax)
+                    data_comp = CompositeSimple(original_data=data, index=data_new,
+                                                mmin=mmin, mmax=mmax)
+
+                    comp = data_comp - neutro_comp
+                    comp = comp.expand_dims(time=[a])
+                    comp.to_netcdf('/datos/luciano.andrian/ncfiles/nc_comps/' + 'Comps_' +
+                                   str(int(a)) + '_' + season_name + '.nc')
+                    del comp
 
 
+            aux = xr.open_dataset(nc_date_dir + 'Composite_' + i + '_2020_' + s + '.nc')
+            neutro = aux.Neutral
+            data_to_concat = aux[c]
+            del aux
 
-        # sim_pos = aux.DMI_sim_pos
-        # sim_neg = aux.DMI_sim_neg
-        #
-        # dmi_un_pos = aux.DMI_un_pos
-        # dmi_un_neg = aux.DMI_un_neg
-        # dmi_pos = aux.DMI_pos
-        # dmi_neg = aux.DMI_neg
-        #
-        # n34_un_pos = aux.N34_un_pos
-        # n34_un_neg = aux.N34_un_neg
-        # n34_pos = aux.N34_pos
-        # n34_neg = aux.N34_neg
-        #
-        # neutro_sim_pos = np.concatenate([neutro.values, sim_pos])
-        #
-        # del aux
+            M = NumberPerts(data_to_concat, neutro, 100)
 
+            if data_to_concat[0] != 0:
+                neutro_concat = np.concatenate([neutro, data_to_concat])
+                from multiprocessing.pool import ThreadPool
+                pool = ThreadPool(25)
+                pool.map(PermuDatesComposite, [n for n in M])
+                pool.close()
 
-    #
+            aux = xr.open_mfdataset('/datos/luciano.andrian/ncfiles/nc_comps/*.nc', parallel=True
+                                    , chunks=10,
+                                    combine='nested', concat_dim="time", coords="different",
+                                    compat="broadcast_equals")
 
+            qt = aux.load().quantile([.05, .95], dim='time', interpolation='linear')
 
-
-
-
-
-
-###########################################
-
-
-
-w_dir = '/home/luciano.andrian/doc/salidas/'
-out_dir = '/home/luciano.andrian/doc/salidas/ENSO_IOD/composite/'
-
-
-import multiprocessing as mp
+            qt.to_netcdf('/datos/luciano.andrian/ncfiles/nc_quantiles/' +
+                         variable + '_quantiles_'+ c + '_' + i + '-2020.nc')
 
 ########################################################################################################################
-variables = 'hgt200'
-variables_t_pp = ['pp_20CR-V3', 'pp_gpcc', 'pp_PREC', 'pp_chirps', 'pp_CMAP',
-                  'pp_gpcp', 't_20CR-V3', 't_cru', 't_BEIC', 't_ghcn_cams', 't_hadcrut', 't_era20c']
-interp = [False, False, False, False, False, False, False, False, False, False, False, False]
-
-seasons = [7, 9, 10]
-seasons_name = ['JJA', 'JAS', 'ASO', 'SON']
-
-two_variables = True
-contour = True
-SA = True
-step = 1
-contour0 = False
-
-scales_pp_t = [np.linspace(-30, 30, 13),  # pp
-               np.linspace(-1, 1, 21)]  # t
-scale = np.linspace(-450, 450, 13)
-
-from matplotlib import colors
-
-cbar_r = colors.ListedColormap(['#B9391B', '#CD4838', '#E25E55', '#F28C89', '#FFCECC',
-                                'white',
-                                '#B3DBFF', '#83B9EB', '#5E9AD7', '#3C7DC3', '#2064AF'])
-cbar_r.set_under('#9B1C00')
-cbar_r.set_over('#014A9B')
-cbar_r.set_bad(color='white')
-
-cbar = colors.ListedColormap(['#B9391B', '#CD4838', '#E25E55', '#F28C89', '#FFCECC',
-                              'white',
-                              '#B3DBFF', '#83B9EB', '#5E9AD7', '#3C7DC3', '#2064AF'][::-1])
-cbar.set_over('#9B1C00')
-cbar.set_under('#014A9B')
-cbar.set_bad(color='white')
-
-cmap = ['BrBG', cbar]
-
-save = True
-full_season = True
-m = 9
-season = 9
-bwa = False
-SA_map = True
-
-
-
-
-
-
-
-
-
-
-
-
